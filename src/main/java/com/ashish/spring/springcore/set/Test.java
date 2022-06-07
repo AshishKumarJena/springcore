c4fefca87e4f521a5d690a17d4373974e7e33bcb
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-07 09:32:56 +0530

    New Commit

--- src/main/java/com/ashish/spring/springcore/set/Test.java
+++ src/main/java/com/ashish/spring/springcore/set/Test.java
@@ -7,10 +7,10 @@ public class Test {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/ashish/spring/springcore/set/setconfig.xml");
-        CarDealer hospital = (CarDealer)context.getBean("cardealer");
-        System.out.println(hospital.getName());
-        System.out.println(hospital.getModels());
-        System.out.println(hospital.getModels().getClass());
+        CarDealer cardealer = (CarDealer)context.getBean("cardealer");
+        System.out.println(cardealer.getName());
+        System.out.println(cardealer.getModels());
+        System.out.println(cardealer.getModels().getClass());
     }
     
 }
